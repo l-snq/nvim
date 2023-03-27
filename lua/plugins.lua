3342b1f3f85b5526c6676eb770cfd25a10f52a89
tastetest <tastytothesupreme@gmail.com>
2023-03-26 20:44:01 -0600

    replaced markdown previewer with a better plugin that works

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -86,7 +86,12 @@ require('packer').startup(function(use)
   }
 
   -- markdown previewer
-  use {"ellisonleao/glow.nvim", config = function() require("glow").setup() end}
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
+  use({"iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" },})
 
   -- Git related plugins
   use 'tpope/vim-fugitive'
