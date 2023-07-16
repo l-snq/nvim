2683258ee3d1eb97374676a35b127d75defe3645
tastetest <tastytothesupreme@gmail.com>
2023-07-16 16:37:54 -0600

    fixed a bunch of things

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -3,7 +3,7 @@ vim.g.miramare_transparent_background = true
 vim.g.miramare_enable_italic_string = true
 vim.g.seoul256_background = 244
 
-vim.cmd [[colorscheme mojave]]
+vim.cmd [[colorscheme lvim]]
 
 --[[vim.cmd[[
   autocmd vimenter * hi Normal guibg=NONE ctermbg=NONE
