c61d84fa1110b10bb8568951f5e094fbcc9d8c2d
ttj <tastytothesupreme@gmail.com>
2024-05-19 11:51:25 -0600

    removed base16

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
 	{"gregsexton/muon"},
 	{"junegunn/seoul256.vim"},
-	{"chriskempson/base16-vim"},
 	{
 		"LazyVim/LazyVim",
-		opts = { colorscheme = "muon"},
+		opts = { colorscheme = "quiet"},
 	},
 }
