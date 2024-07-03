bef5bd8658068b60a43a17dea422d80c60f7dee1
ttjdesktop <tastytothesupreme@gmail.com>
2024-07-02 20:58:56 -0600

    new colors to play w

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,8 @@
 return {
 	{"junegunn/seoul256.vim"},
+	{"znake/znake-vim"},
+	{"vim-scripts/chlordane.vim"},
+	{"itchyny/landscape.vim"},
 	{
 		"LazyVim/LazyVim",
 		opts = { colorscheme = "default"},
