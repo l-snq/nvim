791aaaca472fc431fdb462985672bb0a4ef3498a
lsnq <tastytothesupreme@gmail.com>
2024-08-17 14:33:49 -0600

    updated colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
 	{"junegunn/seoul256.vim"},
 	{
-		"iagorrr/noctishc.nvim",
+		"agude/vim-eldar",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 		   -- load the colorscheme here
-		   vim.cmd([[colorscheme noctishc]])
+		   vim.cmd([[colorscheme eldar]])
 		end,
 	},
 	{"vim-scripts/chlordane.vim"},
