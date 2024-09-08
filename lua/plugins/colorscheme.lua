9eedd989cc5d1c0fa2e42a20f2bed2970637e7c4
lsnq <tastytothesupreme@gmail.com>
2024-09-08 16:09:25 -0600

    updated colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 		   -- load the colorscheme here
-		   vim.cmd([[colorscheme eldar]])
+		   vim.cmd([[colorscheme chlordane]])
 		end,
 	},
 	{"vim-scripts/chlordane.vim"},
