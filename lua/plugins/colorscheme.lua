8660f9fc92c5a2232c360cfee888e06556ef882e
lsnq <tastytothesupreme@gmail.com>
2024-09-15 20:08:20 -0600

    updated colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,11 @@
 return {
 	{"junegunn/seoul256.vim"},
 	{
-		"agude/vim-eldar",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 		   -- load the colorscheme here
-		   vim.cmd([[colorscheme chlordane]])
+		   vim.cmd([[colorscheme quiet]])
 		end,
 	},
 	{"vim-scripts/chlordane.vim"},
